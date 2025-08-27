3c63ce966511965a3c73e4143abf0249d685a4fd
Vdev <vili1120@proton.me>
2025-08-27 21:40:34 +0200

    Updated logger.go

--- dev/logger.go
+++ dev/logger.go
@@ -6,25 +6,25 @@ import (
 )
 
 func Info(msg string, a ...string) {
-	fmt.Printf("[INFO] - %s", fmt.Sprintf(msg, a))
+	fmt.Printf("[INFO] - %s\n", fmt.Sprintf(msg, a))
 }
 
 func Warning(msg string, a ...string) {
-	fmt.Printf("[WARN] - %s", fmt.Sprintf(msg, a))
+	fmt.Printf("[WARN] - %s\n", fmt.Sprintf(msg, a))
 }
 
 func Error(msg string, a ...string) {
-	fmt.Printf("[ERROR] - %s", fmt.Sprintf(msg, a))
+	fmt.Printf("[ERROR] - %s\n", fmt.Sprintf(msg, a))
 }
 
 func LogInfo(msg string, a ...string) {
-	log.Printf("[INFO] - %s", fmt.Sprintf(msg, a))
+	log.Printf("[INFO] - %s\n", fmt.Sprintf(msg, a))
 }
 
 func LogWarning(msg string, a ...string) {
-	log.Printf("[WARN] - %s", fmt.Sprintf(msg, a))
+	log.Printf("[WARN] - %s\n", fmt.Sprintf(msg, a))
 }
 
 func LogError(msg string, a ...string) {
-	log.Printf("[ERROR] - %s", fmt.Sprintf(msg, a))
+	log.Printf("[ERROR] - %s\n", fmt.Sprintf(msg, a))
 }
